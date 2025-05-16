eabe91cb6c90120d940a456b8baefde1baf467ab
SaraFijołek <147390186+SaraFijolek@users.noreply.github.com>
2025-05-16 14:41:23 +0200

    Update ItemsPage.tsx

--- src/pages/ItemsPage.tsx
+++ src/pages/ItemsPage.tsx
@@ -1,4 +1,4 @@
-// src/pages/ItemsPage.tsx
+
 import React, { useEffect, useState } from "react";
 import { listItems, createItem, modifyItem } from "../api/api";
 import { useAuth } from "../context/AuthContext";
